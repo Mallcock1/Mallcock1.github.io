e85796c2bcc4e09983102417046a930eaed2247e
Matthew Allcock <mallcock1@sheffield.ac.uk>
2017-07-06 15:49:24 +0100

    added NAM17 to conferences
    Nikola version: 7.8.1

--- conf.py
+++ conf.py
@@ -878,11 +878,11 @@ FEED_LINKS_APPEND_QUERY = False
 LICENSE = ""
 # I recommend using the Creative Commons' wizard:
 # https://creativecommons.org/choose/
-# LICENSE = """
-# <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
-# <img alt="Creative Commons License BY-NC-SA"
-# style="border-width:0; margin-bottom:12px;"
-# src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"></a>"""
+#LICENSE = """
+#<a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
+#<img alt="Creative Commons License BY-NC-SA"
+#style="border-width:0; margin-bottom:12px;"
+#src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"></a>"""
 
 # A small copyright notice for the page footer (in HTML).
 # (translatable)
